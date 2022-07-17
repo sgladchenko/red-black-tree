cb0d13191bcccdf4090d72c454d429df6ff06515
Sergei Gladchenko <sergladchenko@gmail.com>
2022-07-17 20:34:44 +0100

    Changes to the test cpp file

--- Makefile
+++ Makefile
@@ -2,5 +2,5 @@ CXX = g++
 
 .PHONY: simple_tests
 simple_tests: source/bst.h tests/simple_tests.cpp
-	g++ -std=c++17 -I source/ tests/simple_tests.cpp -o build/simple_tests
+	g++ -g -std=c++17 -I source/ tests/simple_tests.cpp -o build/simple_tests
 	./build/simple_tests
--- tests/simple_tests.cpp
+++ tests/simple_tests.cpp
@@ -2,42 +2,38 @@
 
 #include "bst.h"
 
-void test1()
+class Tests
 {
-    std::cout << "test1\n";
-
-    sg::bst<int> bst_example;
-    int N = 8;
-    int array[] = {12, 1, 3, 10, 5, 11, 2, 0};
-
-    for (int i = 0; i < N; ++i) { bst_example.insert(i); }
-
-    for (int i = 0; i < N; ++i)
+public:
+    static void test1()
     {
-        std::cout << "bst_example.exists(" << array[i] << "): "
-                  << bst_example.exists(array[i]) << std::endl;
+        std::cout << "test1\n";
+
+        sg::bst<int> bst_example;
+
+        int N = 8;
+        int array[] = {12, 1, 3, 10, 5, 11, 2, 0};
+
+        int N1 = 6;
+        int array1[] = {4, 6, 7, 9, 20, -7};
+
+        for (int i = 0; i < N; ++i) { bst_example.insert(array[i]); }
+        for (int i = 0; i < N; ++i)
+        {
+            std::cout << "bst_example.exists(" << array[N-1 - i] << "): "
+                    << bst_example.exists(array[N-1 - i]) << std::endl;
+        }
+        std::cout << std::endl;
+        for (int i = 0; i < N1; ++i)
+        {
+            std::cout << "bst_example.exists(" << array1[i] << "): "
+                    << bst_example.exists(array1[i]) << std::endl;
+        }
     }
-}
-
-void test2()
-{
-    std::cout << "test2\n";
-
-    sg::bst<int> bst_example;
-    int N = 3;
-    int array[] = {12, 1, 3};
-
-    for (int i = 0; i < N; ++i) { bst_example.insert(i); }
-
-    for (int i = 0; i < N; ++i)
-    {
-        std::cout << "bst_example.exists(" << array[i] << "): "
-                  << bst_example.exists(array[i]) << std::endl;
-    }
-}
+};
 
 int main()
 {
-    test2();
+    Tests::test1();
     return 0;
 }
